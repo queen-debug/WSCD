69c59e10e9150ffdece6e0e0db7a6a48f3d2d888
tehya-debug <97810690+tehya-debug@users.noreply.github.com>
2022-08-21 05:06:25 -0700

    Update wscd.lua

--- wscd.lua
+++ wscd.lua
@@ -1,13 +1,8 @@
-local function download_Client()
+
+do
     for num, addon in ipairs(engine.GetAddons()) do
         local wsid = addon.wsid
 
         resource.AddWorkshop(wsid)
     end
 end
-
-hook.Add("InitPostEntity", "wscd", download_Client)
-
---[[
-    WSCD - Workshop Collection Downloader
-]]
